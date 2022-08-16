bf3d9e0101d8a4da51882cf0320a2472c156dd71
adxiong <260245435@qq.com>
2022-08-16 23:05:16 +0800

    update

--- src/model/service/todo/delete.go
+++ src/model/service/todo/delete.go
@@ -4,6 +4,12 @@
  * @Author: Adxiong
  * @Date: 2022-08-14 23:44:31
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-08-15 00:39:50
+ * @LastEditTime: 2022-08-16 23:05:08
  */
 package todo_service
+
+import "golang.org/x/net/context"
+
+func DeleteByID(ctx context.Context, id uint64) {
+
+}
